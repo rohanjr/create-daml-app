05836a3d48acfb31c1bddeea4ee3d4482260818a
Rohan Jacob-Rao <rohanjr@gmail.com>
2019-12-13 15:59:47 -0500

    GoalProposal contract

--- src/daml/Goal.ts
+++ src/daml/Goal.ts
@@ -26,3 +26,29 @@ export class Goal {
     return jsonConvert.serializeObject<Goal>(goal);
   }
 }
+
+@JsonObject("GoalProposal")
+export class GoalProposal {
+  @JsonProperty("party", Party)
+  party: Party = '';
+  @JsonProperty("pledge", Text)
+  pledge: Text = '';
+  @JsonProperty("witness", Party)
+  witness: Party = '';
+
+  static templateId: TemplateId = {moduleName: "Goal", entityName: "GoalProposal"};
+
+  static fromJSON(json: unknown): GoalProposal {
+    const jsonConvert = new JsonConvert();
+    jsonConvert.valueCheckingMode = ValueCheckingMode.DISALLOW_NULL;
+    return jsonConvert.deserializeObject(json, GoalProposal);
+  }
+
+  static toJSON(goalProp: GoalProposal): unknown {
+    const jsonConvert = new JsonConvert();
+    jsonConvert.valueCheckingMode = ValueCheckingMode.DISALLOW_NULL;
+    // TODO(MH): For some reason the conversion to JSON does not work right now.
+    jsonConvert.operationMode = OperationMode.DISABLE;
+    return jsonConvert.serializeObject<GoalProposal>(goalProp);
+  }
+}
