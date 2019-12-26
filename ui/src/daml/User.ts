e0194f1c0dab4294edec6cfbb7ce7c426d56e072
Rohan Jacob-Rao <rohanjr@gmail.com>
2019-12-26 15:18:57 -0500

    Use new representation of DAML for User

--- ui/src/daml/User.ts
+++ ui/src/daml/User.ts
@@ -1,108 +1,77 @@
-import {JsonObject, JsonProperty, JsonConvert, ValueCheckingMode, OperationMode} from "json2typescript";
-import { Party, Template, TemplateId, Choice } from "../ledger/Types";
+// Not generated, but should be using daml2ts.
+import * as daml from "../ledger/Types";
 import * as jtv from '@mojotech/json-type-validation';
 
-@JsonObject("User.AddFriend")
-class AddFriend {
-  @JsonProperty("friend", Party)
-  friend: Party = '';
+const moduleName = 'User';
+const templateId = (entityName: string): daml.TemplateId => ({moduleName, entityName});
 
-  static template = undefined as unknown as typeof User;
+export type AddFriend = {
+  friend: daml.Party;
+};
+export const AddFriend: daml.Serializable<AddFriend> = ({
+  decoder: () => jtv.object({
+    friend: daml.Party.decoder(),
+  }),
+});
 
-  static choiceName = 'AddFriend';
+export type RemoveFriend = {
+  friend: daml.Party;
+};
+export const RemoveFriend: daml.Serializable<RemoveFriend> = ({
+  decoder: () => jtv.object({
+    friend: daml.Party.decoder(),
+  }),
+});
 
-  static toJSON(addFriend: AddFriend): unknown {
-    const jsonConvert = new JsonConvert();
-    jsonConvert.valueCheckingMode = ValueCheckingMode.DISALLOW_NULL;
-    // TODO(MH): For some reason the conversion to JSON does not work right now.
-    jsonConvert.operationMode = OperationMode.DISABLE;
-    return jsonConvert.serializeObject<AddFriend>(addFriend);
-  }
-}
+export type Delete = {
+};
+export const Delete: daml.Serializable<Delete> = ({
+  decoder: () => jtv.object({
+  }),
+});
 
-@JsonObject("User.RemoveFriend")
-class RemoveFriend {
-  @JsonProperty("friend", Party)
-  friend: Party = '';
-
-  static template = undefined as unknown as typeof User;
-
-  static choiceName = 'RemoveFriend';
-
-  static toJSON(removeFriend: RemoveFriend): unknown {
-    const jsonConvert = new JsonConvert();
-    jsonConvert.valueCheckingMode = ValueCheckingMode.DISALLOW_NULL;
-    // TODO(MH): For some reason the conversion to JSON does not work right now.
-    jsonConvert.operationMode = OperationMode.DISABLE;
-    return jsonConvert.serializeObject<RemoveFriend>(removeFriend);
-  }
-}
-
-@JsonObject("User.Delete")
-class Delete {
-  static template = undefined as unknown as typeof User;
-
-  static choiceName = 'Delete';
-
-  static toJSON(delete_: Delete): unknown {
-    const jsonConvert = new JsonConvert();
-    jsonConvert.valueCheckingMode = ValueCheckingMode.DISALLOW_NULL;
-    // TODO(MH): For some reason the conversion to JSON does not work right now.
-    jsonConvert.operationMode = OperationMode.DISABLE;
-    return jsonConvert.serializeObject<Delete>(delete_);
-  }
-}
-
-@JsonObject("User.Archive")
-class Archive {
-  static template = undefined as unknown as typeof User;
-
-  static choiceName = 'Archive';
-
-  static toJSON(archive_: Archive): unknown {
-    const jsonConvert = new JsonConvert();
-    jsonConvert.valueCheckingMode = ValueCheckingMode.DISALLOW_NULL;
-    // TODO(MH): For some reason the conversion to JSON does not work right now.
-    jsonConvert.operationMode = OperationMode.DISABLE;
-    return jsonConvert.serializeObject<Delete>(archive_);
-  }
-}
-
-@JsonObject("User")
-export class User {
-  @JsonProperty("party", Party)
-  party: Party = '';
-  @JsonProperty("friends", [Party])
-  friends: Party[] = [];
-
-  static templateId: TemplateId = {moduleName: "User", entityName: "User"};
-
-  static fromJSON(json: unknown): User {
-    const jsonConvert = new JsonConvert();
-    jsonConvert.valueCheckingMode = ValueCheckingMode.DISALLOW_NULL;
-    return jsonConvert.deserializeObject(json, User);
-  }
-
-  static toJSON(user: User): unknown {
-    const jsonConvert = new JsonConvert();
-    jsonConvert.valueCheckingMode = ValueCheckingMode.DISALLOW_NULL;
-    // TODO(MH): For some reason the conversion to JSON does not work right now.
-    jsonConvert.operationMode = OperationMode.DISABLE;
-    return jsonConvert.serializeObject<User>(user);
-  }
-
-  static AddFriend = AddFriend;
-
-  static RemoveFriend = RemoveFriend;
-
-  static Delete = Delete;
-}
+export type Archive = {
+};
+export const Archive: daml.Serializable<Archive> = ({
+  decoder: () => jtv.object({
+  }),
+});
 
 
-AddFriend.template = User;
-
-RemoveFriend.template = User;
-
-Delete.template = User;
-
-Archive.template = User;
+export type User = {
+  party: daml.Party;
+  friends: daml.Party[];
+};
+export const User: daml.Template<User> & {
+  Archive: daml.Choice<User, Archive>;
+  Delete: daml.Choice<User, Delete>;
+  AddFriend: daml.Choice<User, AddFriend>;
+  RemoveFriend: daml.Choice<User, RemoveFriend>;
+} = {
+  templateId: templateId('User'),
+  decoder: () => jtv.object({
+    party: daml.Party.decoder(),
+    friends: daml.List(daml.Party).decoder()
+  }),
+  Archive: {
+    template: () => User,
+    choiceName: 'Archive',
+    decoder: Archive.decoder,
+  },
+  Delete: {
+    template: () => User,
+    choiceName: 'Delete',
+    decoder: Delete.decoder,
+  },
+  AddFriend: {
+    template: () => User,
+    choiceName: 'AddFriend',
+    decoder: AddFriend.decoder,
+  },
+  RemoveFriend: {
+    template: () => User,
+    choiceName: 'RemoveFriend',
+    decoder: RemoveFriend.decoder,
+  },
+};
+daml.registerTemplate(User);
