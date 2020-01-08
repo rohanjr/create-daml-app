e0f1901566d5cbf63bf40ef3e542793ae85bacf0
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-01-08 14:51:33 -0500

    Revert code imported from DAVL -- try to minimise changes in PR

--- ui/src/ledger/Credentials.ts
+++ ui/src/ledger/Credentials.ts
@@ -25,19 +25,4 @@ export const computeCredentials = (party: string): Credentials => {
   return {party, token};
 }
 
-/**
- * Check that the party in the token matches the party of the credentials and
- * that the ledger ID in the token matches `LEDGER_ID`.
- */
-export const preCheckCredentials = ({party, token}: Credentials): string | null => {
-  const decoded = decode(token, '', true);
-  if (!decoded.ledgerId || decoded.ledgerId !== LEDGER_ID) {
-    return 'The password is not valid for DAVL.';
-  }
-  if (!decoded.party || decoded.party !== party) {
-    return 'The password is not valid for this user.';
-  }
-  return null;
-}
-
 export default Credentials;
