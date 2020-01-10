08941a7b0e01aa9cfb57a2c5658824669c4b9de2
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-01-10 12:38:14 -0500

    Ledger fetch and json types

---
+++ daml-json-types/src/index.ts
@@ -0,0 +1,234 @@
+// Copyright (c) 2020 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+import * as jtv from '@mojotech/json-type-validation';
+
+/**
+ * Interface for companion objects of serializable types. Its main purpose is
+ * to describe the JSON encoding of values of the serializable type.
+ */
+export interface Serializable<T> {
+  // NOTE(MH): This must be a function to allow for mutually recursive decoders.
+  decoder: () => jtv.Decoder<T>;
+}
+
+/**
+ * This is a check to ensure that enum's are serializable. If the enum is named 'Color', the check
+ * is done by adding a line 'STATIC_IMPLEMENTS_SERIALIZABLE_CHECK<Color>(Color)' after the
+ * definition of 'Color'.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+export const STATIC_IMPLEMENTS_SERIALIZABLE_CHECK = <T>(_: Serializable<T>) => {}
+
+/**
+ * Identifier of a DAML template.
+ */
+export type TemplateId = {
+  packageId: string;
+  moduleName: string;
+  entityName: string;
+}
+
+/**
+ * Interface for objects representing DAML templates. It is similar to the
+ * `Template` type class in DAML.
+ */
+export interface Template<T extends object, K = unknown> extends Serializable<T> {
+  templateId: TemplateId;
+  keyDecoder: () => jtv.Decoder<K>;
+  Archive: Choice<T, {}, {}>;
+}
+
+/**
+ * Interface for objects representing DAML choices. It is similar to the
+ * `Choice` type class in DAML.
+ */
+export interface Choice<T extends object, C, R> {
+  template: () => Template<T>;
+  argumentDecoder: () => jtv.Decoder<C>;
+  resultDecoder: () => jtv.Decoder<R>;
+  choiceName: string;
+}
+
+const registeredTemplates: {[key: string]: Template<object>} = {};
+
+const templateIdToString = ({packageId, moduleName, entityName}: TemplateId) =>
+  `${packageId}:${moduleName}:${entityName}`;
+
+export const registerTemplate = <T extends object>(template: Template<T>) => {
+  const templateId = templateIdToString(template.templateId);
+  const oldTemplate = registeredTemplates[templateId];
+  if (oldTemplate === undefined) {
+    registeredTemplates[templateId] = template;
+    console.debug(`Registered template ${templateId}.`);
+  } else {
+    console.warn(`Trying to re-register template ${templateId}.`);
+  }
+}
+
+export const lookupTemplate = (templateId: TemplateId): Template<object> => {
+  const templateIdStr = templateIdToString(templateId);
+  const template = registeredTemplates[templateIdStr];
+  if (template === undefined) {
+    throw Error(`Trying to look up template ${templateIdStr}.`);
+  }
+  return template;
+}
+
+/**
+ * The counterpart of DAML's `()` type.
+ */
+export type Unit = {};
+
+/**
+ * Companion obect of the `Unit` type.
+ */
+export const Unit: Serializable<Unit> = {
+  decoder: () => jtv.object({}),
+}
+
+/**
+ * The counterpart of DAML's `Bool` type.
+ */
+export type Bool = boolean;
+
+/**
+ * Companion object of the `Bool` type.
+ */
+export const Bool: Serializable<Bool> = {
+  decoder: jtv.boolean,
+}
+
+/**
+ * The counterpart of DAML's `Int` type. We represent `Int`s as string in order
+ * to avoid a loss of precision.
+ */
+export type Int = string;
+
+/**
+ * Companion object of the `Int` type.
+ */
+export const Int: Serializable<Int> = {
+  decoder: jtv.string,
+}
+
+/**
+ * The counterpart of DAML's `Decimal` type. We represent `Decimal`s as string
+ * in order to avoid a loss of precision. The string must match the regular
+ * expression `-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?`.
+ */
+export type Decimal = string;
+
+/**
+ * Companion object of the `Decimal` type.
+ */
+export const Decimal: Serializable<Decimal> = {
+  decoder: jtv.string,
+}
+
+/**
+ * The counterpart of DAML's `Text` type.
+ */
+export type Text = string;
+
+/**
+ * Companion object of the `Text` type.
+ */
+export const Text: Serializable<Text> = {
+  decoder: jtv.string,
+}
+
+/**
+ * The counterpart of DAML's `Time` type. We represent `Times`s as strings with
+ * format `YYYY-MM-DDThh:mm:ss[.ssssss]Z`.
+ */
+export type Time = string;
+
+/**
+ * Companion object of the `Time` type.
+ */
+export const Time: Serializable<Time> = {
+  decoder: jtv.string,
+}
+
+/**
+ * The counterpart of DAML's `Party` type. We represent `Party`s as strings
+ * matching the regular expression `[A-Za-z0-9:_\- ]+`.
+ */
+export type Party = string;
+
+/**
+ * Companion object of the `Party` type.
+ */
+export const Party: Serializable<Party> = {
+  decoder: jtv.string,
+}
+
+/**
+ * The counterpart of DAML's `[T]` list type. We represent lists using arrays.
+ */
+export type List<T> = T[];
+
+/**
+ * Companion object of the `List` type.
+ */
+export const List = <T>(t: Serializable<T>): Serializable<T[]> => ({
+  decoder: () => jtv.array(t.decoder()),
+});
+
+/**
+ * The counterpart of DAML's `Date` type. We represent `Date`s as strings with
+ * format `YYYY-MM-DD`.
+ */
+export type Date = string;
+
+/**
+ * Companion object of the `Date` type.
+ */
+export const Date: Serializable<Date> = {
+  decoder: jtv.string,
+}
+
+/**
+ * The counterpart of DAML's `ContractId T` type. We represent `ContractId`s
+ * as strings. Their exact format of these strings depends on the ledger the
+ * DAML application is running on.
+ */
+export type ContractId<T> = string;
+
+/**
+ * Companion object of the `ContractId` type.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const ContractId = <T>(_t: Serializable<T>): Serializable<ContractId<T>> => ({
+  decoder: jtv.string,
+});
+
+/**
+ * The counterpart of DAML's `Optional T` type. Nested optionals are not yet
+ * supported.
+ */
+export type Optional<T> = T | null;
+
+/**
+ * Companion object of the `Optional` type.
+ */
+export const Optional = <T>(t: Serializable<T>): Serializable<Optional<T>> => ({
+  decoder: () => jtv.oneOf(jtv.constant(null), t.decoder()),
+});
+
+/**
+ * The counterpart of DAML's `TextMap T` type. We represent `TextMap`s as
+ * dictionaries.
+ */
+export type TextMap<T> = { [key: string]: T };
+
+/**
+ * Companion object of the `TextMap` type.
+ */
+export const TextMap = <T>(t: Serializable<T>): Serializable<TextMap<T>> => ({
+  decoder: () => jtv.dict(t.decoder()),
+});
+
+// TODO(MH): `Numeric` type.
+
+// TODO(MH): `Map` type.
---
+++ daml-ledger-fetch/src/index.ts
@@ -0,0 +1,237 @@
+// Copyright (c) 2020 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { Choice, ContractId, List, Party, Template, TemplateId, Text, lookupTemplate } from '@digitalasset/daml-json-types';
+import * as jtv from '@mojotech/json-type-validation';
+import fetch from 'cross-fetch';
+
+export type CreateEvent<T extends object, K = unknown> = {
+  templateId: TemplateId;
+  contractId: ContractId<T>;
+  signatories: List<Party>;
+  observers: List<Party>;
+  agreementText: Text;
+  key: K;
+  payload: T;
+}
+
+export type ArchiveEvent<T extends object> = {
+  templateId: TemplateId;
+  contractId: ContractId<T>;
+}
+
+export type Event<T extends object, K = unknown> =
+  | { created: CreateEvent<T, K> }
+  | { archived: ArchiveEvent<T> }
+
+const decodeTemplateId: jtv.Decoder<TemplateId> = jtv.object({
+  packageId: jtv.string(),
+  moduleName: jtv.string(),
+  entityName: jtv.string(),
+});
+
+const decodeCreateEvent = <T extends object, K>(template: Template<T, K>): jtv.Decoder<CreateEvent<T, K>> => jtv.object({
+  templateId: decodeTemplateId,
+  contractId: ContractId(template).decoder(),
+  signatories: List(Party).decoder(),
+  observers: List(Party).decoder(),
+  agreementText: Text.decoder(),
+  key: template.keyDecoder(),
+  payload: template.decoder(),
+});
+
+const decodeCreateEventUnknown: jtv.Decoder<CreateEvent<object>> =
+  jtv.valueAt(['templateId'], decodeTemplateId).andThen((templateId) =>
+    decodeCreateEvent(lookupTemplate(templateId))
+  );
+
+const decodeArchiveEventUnknown: jtv.Decoder<ArchiveEvent<object>> = jtv.object({
+  templateId: decodeTemplateId,
+  contractId: ContractId({decoder: jtv.unknownJson}).decoder(),
+});
+
+const decodeEventUnknown: jtv.Decoder<Event<object>> = jtv.oneOf<Event<object>>(
+  jtv.object({created: decodeCreateEventUnknown}),
+  jtv.object({archived: decodeArchiveEventUnknown}),
+);
+
+/**
+ * Type for queries against the `/contract/search` endpoint of the JSON API.
+ * `Query<T>` is the type of queries that are valid when searching for
+ * contracts of template type `T`.
+ *
+ * Comparison queries are not yet supported.
+ *
+ * NB: This type is heavily related to the `DeepPartial` type that can be found
+ * in the TypeScript community.
+ */
+export type Query<T> = T extends object ? {[K in keyof T]?: Query<T[K]>} : T;
+// TODO(MH): Support comparison queries.
+
+
+type LedgerResponse = {
+  status: number;
+  result: unknown;
+}
+
+type LedgerError = {
+  status: number;
+  errors: string[];
+}
+
+const decodeLedgerResponse: jtv.Decoder<LedgerResponse> = jtv.object({
+  status: jtv.number(),
+  result: jtv.unknownJson(),
+});
+
+const decodeLedgerError: jtv.Decoder<LedgerError> = jtv.object({
+  status: jtv.number(),
+  errors: jtv.array(jtv.string()),
+});
+
+/**
+ * An object of type `Ledger` represents a handle to a DAML ledger.
+ */
+class Ledger {
+  private readonly token: string;
+  private readonly baseUrl: string;
+
+  constructor(token: string, baseUrl?: string) {
+    this.token = token;
+    if (!baseUrl) {
+      this.baseUrl = '';
+    } else if (baseUrl.endsWith('/')) {
+      this.baseUrl = baseUrl;
+    } else {
+      throw Error(`The ledger base URL must end in a '/'. (${baseUrl})`);
+    }
+  }
+
+  /**
+   * Internal function to submit a command to the JSON API.
+   */
+  private async submit(endpoint: string, payload: unknown): Promise<unknown> {
+    const httpResponse = await fetch(this.baseUrl + endpoint, {
+      body: JSON.stringify(payload),
+      headers: {
+        'Authorization': 'Bearer ' + this.token,
+        'Content-type': 'application/json'
+      },
+      method: 'post',
+    });
+    const json = await httpResponse.json();
+    if (!httpResponse.ok) {
+      console.log(json);
+      throw jtv.Result.withException(decodeLedgerError.run(json));
+    }
+    const ledgerResponse = jtv.Result.withException(decodeLedgerResponse.run(json));
+    return ledgerResponse.result;
+  }
+
+  /**
+   * Retrieve all contracts for a given template which match a query. See
+   * https://github.com/digital-asset/daml/blob/master/docs/source/json-api/search-query-language.rst
+   * for a description of the query language.
+   */
+  async query<T extends object, K>(template: Template<T, K>, query: Query<T>): Promise<CreateEvent<T, K>[]> {
+    const payload = {"%templates": [template.templateId], ...query};
+    const json = await this.submit('contracts/search', payload);
+    return jtv.Result.withException(jtv.array(decodeCreateEvent(template)).run(json));
+  }
+
+  /**
+   * Retrieve all contracts for a given template.
+   */
+  async fetchAll<T extends object, K>(template: Template<T, K>): Promise<CreateEvent<T, K>[]> {
+    return this.query(template, {} as Query<T>);
+  }
+
+  async lookupByKey<T extends object, K>(template: Template<T, K>, key: K extends undefined ? never : K): Promise<CreateEvent<T, K> | null> {
+    const payload = {
+      templateId: template.templateId,
+      key,
+    };
+    const json = await this.submit('contracts/lookup', payload);
+    return jtv.Result.withException(jtv.oneOf(jtv.constant(null), decodeCreateEvent(template)).run(json));
+  }
+
+  /**
+   * Mimic DAML's `lookupByKey`. The `key` must be a formulation of the
+   * contract key as a query.
+   */
+  async pseudoLookupByKey<T extends object, K>(template: Template<T, K>, key: Query<T>): Promise<CreateEvent<T, K> | undefined> {
+    const contracts = await this.query(template, key);
+    if (contracts.length > 1) {
+      throw Error("pseudoLookupByKey: query returned multiple contracts");
+    }
+    return contracts[0];
+  }
+
+  /**
+   * Mimic DAML's `fetchByKey`. The `key` must be a formulation of the
+   * contract key as a query.
+   */
+  async pseudoFetchByKey<T extends object, K>(template: Template<T, K>, key: Query<T>): Promise<CreateEvent<T, K>> {
+    const contract = await this.pseudoLookupByKey(template, key);
+    if (contract === undefined) {
+      throw Error("pseudoFetchByKey: query returned no contract");
+    }
+    return contract;
+  }
+
+  /**
+   * Create a contract for a given template.
+   */
+  async create<T extends object, K>(template: Template<T, K>, argument: T): Promise<CreateEvent<T, K>> {
+    const payload = {
+      templateId: template.templateId,
+      argument,
+    };
+    const json = await this.submit('command/create', payload);
+    return jtv.Result.withException(decodeCreateEvent(template).run(json));
+  }
+
+  /**
+   * Exercise a choice on a contract.
+   */
+  async exercise<T extends object, C, R>(choice: Choice<T, C, R>, contractId: ContractId<T>, argument: C): Promise<[R , Event<object>[]]> {
+    const payload = {
+      templateId: choice.template().templateId,
+      contractId,
+      choice: choice.choiceName,
+      argument,
+    };
+    const json = await this.submit('command/exercise', payload);
+    // Decode the server response into a tuple.
+    const responseDecoder: jtv.Decoder<{exerciseResult: R; contracts: Event<object>[]}> = jtv.object({
+      exerciseResult: choice.resultDecoder(),
+      contracts: jtv.array(decodeEventUnknown),
+    });
+    const {exerciseResult, contracts} = jtv.Result.withException(responseDecoder.run(json));
+    return [exerciseResult, contracts];
+  }
+
+  /**
+   * Mimic DAML's `exerciseByKey`. The `key` must be a formulation of the
+   * contract key as a query.
+   */
+  async pseudoExerciseByKey<T extends object, C, R>(choice: Choice<T, C, R>, key: Query<T>, argument: C): Promise<[R, Event<object>[]]> {
+    const contract = await this.pseudoFetchByKey(choice.template(), key);
+    return this.exercise(choice, contract.contractId, argument);
+  }
+
+  /**
+   * Archive a contract given by its contract id.
+   */
+  async archive<T extends object>(template: Template<T>, contractId: ContractId<T>): Promise<unknown> {
+    return this.exercise(template.Archive, contractId, {});
+  }
+
+  /**
+   * Archive a contract given by its contract id.
+   */
+  async pseudoArchiveByKey<T extends object>(template: Template<T>, key: Query<T>): Promise<unknown> {
+    return this.pseudoExerciseByKey(template.Archive, key, {});
+  }
+}
+
+export default Ledger;
